753e0a1b51d3e44d2f382c68e9d09f472e8083dc
jzilg <johannes.zilg@tausendkind.de>
2016-04-11 16:41:00 +0200

    JavaScript for Form-functionalaty

---
+++ js/form.js
@@ -0,0 +1,28 @@
+(function($form) {
+
+    var $inputs = $form.find(':input');
+
+    $inputs.each(function() {
+
+        var $input = $(this),
+            $label = $input.parent('label');
+
+        if ($input.val())
+            $label.addClass('active')
+
+        $input.focusin(function() {
+            $label.addClass('focus');
+        });
+
+        $input.change(function() {
+            $label.addClass('active')
+        });
+
+        $input.focusout(function() {
+            $label.removeClass('focus');
+
+            if (!$input.val())
+                $label.removeClass('active')
+        });
+    });
+})($('form'))
